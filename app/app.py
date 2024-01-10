23a613366b8dbb4d866e23326e42dd8caa93e4c8
omarkhaled646 <63152184+omarkhaled646@users.noreply.github.com>
2024-01-10 12:52:11 +0200

    repo cleansing

--- app/app.py
+++ app/app.py
@@ -8,32 +8,85 @@ app = Flask(__name__)
 
 class AddressVerifier:
     def __init__(self, model_path, word_index_path):
+        """
+        Initialize the AddressVerifier class.
+
+        Parameters:
+        - model_path (str): Path to the ONNX model file.
+        - word_index_path (str): Path to the word index JSON file.
+        """
         self.session = onnxruntime.InferenceSession(model_path)
         self.word_index = self.load_word_index(word_index_path)
 
     def load_word_index(self, word_index_path):
+        """
+        Load the word index from a JSON file.
+
+        Parameters:
+        - word_index_path (str): Path to the word index JSON file.
+
+        Returns:
+        - dict: Loaded word index.
+        """
         with open(word_index_path, 'r') as f:
             word_index = json.load(f)
         return word_index
 
     def preprocess_text(self, text):
+        """
+        Preprocess the input text by tokenizing and padding it.
+
+        Parameters:
+        - text (str): Input text to be preprocessed.
+
+        Returns:
+        - list: Padded sequence of word indices.
+        """
         tokenized_text = text.split()
         indexed_text = [self.word_index.get(token, 0) for token in tokenized_text]
         padded_sequence = self.pad_sequence(indexed_text, max_length)
         return padded_sequence
 
     def pad_sequence(self, sequence, max_length):
+        """
+        Pad the input sequence to a specified maximum length.
+
+        Parameters:
+        - sequence (list): Input sequence.
+        - max_length (int): Maximum length for padding.
+
+        Returns:
+        - list: Padded sequence.
+        """
         padded_sequence = sequence[:max_length] + [0] * max(0, max_length - len(sequence))
         return padded_sequence
 
     def predict_address(self, text):
+        """
+        Make a prediction on the input text using the loaded model.
+
+        Parameters:
+        - text (str): Input text for prediction.
+
+        Returns:
+        - np.array: Prediction result.
+        """
         preprocessed_data = self.preprocess_text(text)
         input_name = self.session.get_inputs()[0].name
         output_name = self.session.get_outputs()[0].name
         prediction = self.session.run([output_name], {input_name: np.array([preprocessed_data], dtype=np.float32)})[0]
         return prediction
-    
+
     def process_address(self, address_text):
+        """
+        Process the input address text, make a prediction, and return the result.
+
+        Parameters:
+        - address_text (str): Input address text.
+
+        Returns:
+        - dict: Result dictionary containing 'is_in_cairo' and 'confidence'.
+        """
         start_time = time.time()
         prediction = self.predict_address(address_text)
 
@@ -52,7 +105,6 @@ class AddressVerifier:
 
         return result
 
-
 model_path = '/model/model.onnx'
 word_index_path = '/model/word_index.json'
 max_length = 3  
@@ -61,6 +113,17 @@ address_verifier = AddressVerifier(model_path, word_index_path)
 
 @app.route('/verify_cairo_address', methods=['POST'])
 def verify_address():
+    """
+    Endpoint to verify if an address is in Cairo.
+
+    Expected JSON payload:
+    {
+        "address": "Input address text"
+    }
+
+    Returns:
+    - JSON: Result dictionary containing 'is_in_cairo' and 'confidence'.
+    """
     data = request.get_json()
     address_text = data.get('address', '')
 
@@ -69,4 +132,4 @@ def verify_address():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host= '0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
--- core model architecture/onnx_model_creation.py
+++ core model architecture/onnx_model_creation.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 import tf2onnx
 import onnx
 
-tf_model = tf.keras.models.load_model('./models/lstm_model.h5')
+tf_model = tf.keras.models.load_model('./model/lstm_model.h5')
 
 # Convert the TensorFlow model to ONNX format
 onnx_model, _ = tf2onnx.convert.from_keras(tf_model)
 
 # Save the ONNX model to a file
-onnx_model_path = './models/model.onnx'
+onnx_model_path = './model/model.onnx'
 onnx_model, _ = tf2onnx.convert.from_keras(tf_model, opset=13)
 onnx.save(onnx_model, onnx_model_path)
